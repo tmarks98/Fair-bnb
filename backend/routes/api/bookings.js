af12fd509de0c4463fa450a5ef1a2ded4067ca68
Tyler <tyleramarks@gmail.com>
2023-05-22 04:17:48 -0400

    feat:  add bookings route

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,76 @@
+const router = require("express").Router();
+const { Sequelize } = require("sequelize");
+const { Spot, SpotImage, Booking } = require("../../db/models");
+const {
+  requireAuth,
+  throwIfNull,
+  checkAuthorization,
+} = require("../../utils/auth");
+const { validBooking } = require("../../utils/validation");
+
+router.get("/current", requireAuth, async (req, res) => {
+  const bookings = await Booking.findAll({
+    where: {
+      userId: req.user.id,
+    },
+    include: [
+      { model: Spot, attributes: { exclude: ["createdAt", "updatedAt"] } },
+    ],
+  });
+  const previews = await SpotImage.findAll();
+  const Bookings = bookings.map((booking) => {
+    const previewImageObj = previews.find((preview) => {
+      return booking.spotId === preview.spotIdd;
+    });
+    const previewImage = previewImageObj ? previewImageObj.url : null;
+    return {
+      ...booking.toJSON(),
+      Spot: {
+        ...booking.Spot.toJSON(),
+        previewImage,
+      },
+    };
+  });
+  res.json({ Bookings });
+});
+
+const getToday = () => {
+  const date = new Date();
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+};
+
+router.put("/:bookingId", requireAuth, async (req, res) => {
+  const booking = await Booking.findByPk(req.params.bookingId);
+  throwIfNull(booking, "Booking");
+  checkAuthorization(booking.userId === req.user.id);
+  await validBooking(
+    booking.startDate,
+    booking.endDate,
+    booking.spotId,
+    booking.id
+  );
+  await booking.update(req.body);
+  res.json(booking);
+});
+
+router.delete("/:bookingId", requireAuth, async (req, res) => {
+  const booking = await Booking.findByPk(req.params.bookingId);
+  throwIfNull(booking, "Booking");
+  const spot = await Spot.findByPk(booking.spotId);
+  throwIfNull(spot);
+
+  checkAuthorization(
+    booking.userId === req.user.id || spot.ownerId === req.user.id
+  );
+  const bookingConflict =
+    booking.startDate <= getToday() && booking.endDate >= getToday();
+  if (bookingConflict) {
+    throw {
+      status: 402,
+      message: "Bookings that have been started cant be deleted",
+    };
+  }
+  await Booking.destroy({ where: { id: req.params.bookingId } });
+  res.json({ message: "Successfully deleted" });
+});
+module.exports = router;
