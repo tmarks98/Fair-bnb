3a96ca9d9b30c948bdaeae9e186d857e51060f48
Tyler <tyleramarks@gmail.com>
2023-08-07 01:26:42 -0400

    fsfs

--- frontend/src/components/SpotPreview/index.js
+++ frontend/src/components/SpotPreview/index.js
@@ -8,7 +8,7 @@ export default function SpotPreview(props) {
   let spot = props.spot;
   let [deleteMod, setDeleteMod] = useState(false);
   const history = useHistory();
-  const spotRating = spot.avgRating.toFixed(2)
+  const spotRating = spot?.avgRating?.toFixed(2)
   if (!spot) return null;
   let id = spot.id;
 
