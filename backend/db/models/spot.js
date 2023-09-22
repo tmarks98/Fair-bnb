528944a57105ed12236af66eb4ea553306b5ec11
Tyler <tyleramarks@gmail.com>
2023-09-22 13:41:28 -0400

    fixed seed errors

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -53,7 +53,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
       },
       price: {
